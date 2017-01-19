851cca3b0cb460454949d6e1646016e19e54f233
Matt Lewis <matthew.lewis@pagesuite.com>
2017-01-19 14:51:53 +0000

    add exercise 7

---
+++ 07-http-client.js
@@ -0,0 +1,17 @@
+/*
+    Write a program that performs an HTTP GET request to a URL provided to you
+    as the first command-line argument. Write the String contents of each
+    "data" event from the response to a new line on the console (stdout).
+*/
+
+'use strict';
+
+const http = require('http');
+
+const url = process.argv[2];
+
+http.get(url, response => {
+    response.setEncoding('utf8');
+    response.on('data', data => console.log(data));
+    response.on('error', err => console.error(err));    
+});
