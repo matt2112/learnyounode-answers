0404cf5a4e0011b6f7cb244f9bd6f66ec5564764
Matt Lewis <matthew.lewis@pagesuite.com>
2017-01-25 15:52:37 +0000

    add missing instructions

--- 01-hello-world.js
+++ 01-hello-world.js
@@ -1,3 +1,4 @@
-// Write a program that prints the text "HELLO WORLD" to the console
-// (stdout).
+/*
+    Write a program that prints the text "HELLO WORLD" to the console (stdout).
+*/
 console.log("HELLO WORLD");
--- 02-baby-steps.js
+++ 02-baby-steps.js
@@ -1,5 +1,7 @@
-//  Write a program that accepts one or more numbers as command-line arguments
-//  and prints the sum of those numbers to the console (stdout).
+/*
+    Write a program that accepts one or more numbers as command-line arguments
+    and prints the sum of those numbers to the console (stdout).
+*/
 
 const args = process.argv.slice(2);
 const sum = args.reduce((a, b) => Number(a) + Number(b));
